a6dddc82efc7b3a0876cdee5d4c42187040794c5
Isaac <Isaacthedeveloper@gmail.com>
2019-10-27 18:42:23 -0400

    Initial Commit

---
+++ CheckSplitSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheckSplitSwiftUI
+//
+//  Created by Isaac Ballas on 2019-10-27.
+//  Copyright © 2019 Isaacballas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
